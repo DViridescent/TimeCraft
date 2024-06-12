f683233792ffb0a2679b39522c6830a83c83dfc8
呆维iridescent <475430703@qq.com>
2024-06-12 14:49:44 +0800

    add AnimatedBorder

---
+++ CustomControls/AnimatedBorder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomControls;
+internal class AnimatedBorder : Border
+{
+    static AnimatedBorder()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(AnimatedBorder), new FrameworkPropertyMetadata(typeof(AnimatedBorder)));
+    }
+}
